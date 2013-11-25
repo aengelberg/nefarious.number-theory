f0fcc4e99090301f989f6264bf710f9dcab6ec47
Frances Davies <narfyspeaks@gmail.com>
2013-11-25 14:48:54 -0800

    moved test cases

--- test/nefarious/number_theory/quadratic_test.clj
+++ test/nefarious/number_theory/quadratic_test.clj
@@ -2,4 +2,10 @@
     (:use clojure.test
         nefarious.number-theory.core))
 
-(is (= (quadratic-solutions
+(deftest quadratic-test
+  (is (or (= (quadratic-solutions  1 1 -6) '(-3 2)) (= (quadratic-solutions  1 1 -6) '(2 -3))))
+  (is (= (quadratic-solutions 1 6 9) '(-3)))
+  (is (= (quadratic-solutions 2 1 1)) '())
+  (is (= (quadratic-integer-solutions 2 5 -3)) '(-3))
+  (is (= (quadratic-natural-solutions  1 1 -6) '(2)))
+  (is (= (quadratic-natural-solutions 2 5 -3) '())))
